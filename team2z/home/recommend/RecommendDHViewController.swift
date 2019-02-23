126de07b0bc86c49b706d8df68105e542be41244
Jung Inho <area1211@naver.com>
2019-02-23 12:51:42 +0900

    스플래쉬 이미지 해결합시다..

---
+++ team2z/home/recommend/RecommendDHViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecommendDHViewController.swift
+//  team2z
+//
+//  Created by 정인호 on 23/02/2019.
+//  Copyright © 2019 TeamNexters. All rights reserved.
+//
+
+import UIKit
+
+class RecommendDHViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
