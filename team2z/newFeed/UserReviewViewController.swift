53dbc3186de2b8f777f3fbb44f836214b814e7de
munjung <jekang0123@gmail.com>
2019-02-26 16:06:34 +0900

    ..

---
+++ team2z/newFeed/UserReviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserReviewViewController.swift
+//  team2z
+//
+//  Created by MOONJUNG on 23/02/2019.
+//  Copyright © 2019 TeamNexters. All rights reserved.
+//
+
+import UIKit
+
+class UserReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
